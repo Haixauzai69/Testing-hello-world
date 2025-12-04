2d3b911fcd9af1c07e97f0aabb4df1c9be8f63ad
HaiPH <HaiPH@PHAMHOANGHAI>
2025-12-04 15:16:40 +0100

    weird aufgabe

--- HelloWorld/src/display.cpp
+++ HelloWorld/src/display.cpp
@@ -7,18 +7,28 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-using namespace std;
+
 
 int main() {
-    cout << setiosflags(ios::left)
-         << setw(5) << "ID"
-         << setw(10) << "Name"
-         << setw(20) << "MaxGeschwindigkeit"
-         << setw(15) << "Gesamtstrecke"
-         << resetiosflags(ios::left)  // reset to normal
-         << endl;
+//    cout << setiosflags(ios::left)
+//         << setw(5) << "ID"
+//         << setw(10) << "Name"
+//         << setw(20) << "MaxGeschwindigkeit"
+//         << setw(15) << "Gesamtstrecke"
+//         << resetiosflags(ios::left)  // reset to normal
+//         << endl;
+//
+//    cout << string(50, '-') << endl;
+	std::cout << "I love you too!" << std::endl;
 
-    cout << string(50, '-') << endl;
+	for (int i = 0; i < 1000 ; i++)
+	{
+		if ((i % 5 == 3) && (i % 7 == 4) && (i % 9 == 5))
+		{
+			std::cout << "Answer: " << i << std::endl;
+			return i;
+		}
+	}
 }
 
 
